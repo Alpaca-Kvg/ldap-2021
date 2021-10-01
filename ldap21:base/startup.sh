ba266c7692d574e5cbcb8fb1fcadb7caf4bd8b06
isx23932486 <isx23932486@i09.informatica.escoladeltreball.org>
2021-10-01 12:02:36 +0200

    base

--- ldap21:base/startup.sh
+++ ldap21:base/startup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# export DEBIAN_FRONTEND=noninteractive
+# apt-get -y install slapd
+
 rm -rf /etc/ldap/slapd.d/*
 rm -rf /var/lib/ldap/*
-slaptest -f slapd.conf -F /etc/ldap/slapd.d
-slapadd -F /etc/ldap/slapd.d/ -l organitzacio.ldif
-chown -R openldap.openldap /etc/ldap/slapd.d/ /var/lib/ldap/
-/usr/sbin/slapd
-#injeccion usuaris
-#ldapadd -x -h 172.17.0.2 -D "cn=Manager,dc=edt,dc=org" -w secret -f users.ldif 
+slaptest -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d
+slapadd  -F /etc/ldap/slapd.d -l /opt/docker/organitzacio-edt.org.ldif
+chown -R openldap.openldap /etc/ldap/slapd.d /var/lib/ldap
+/usr/sbin/slapd -d0
+
+
+# Pendent:
+# configuració client
+# posar totes les dades en una injecció massiva inicial
+
